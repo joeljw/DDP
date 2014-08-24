e9fdc3c850fe2dbf7269dca4fd0960b01ce5e2a3
Joel W <joeljw@gmail.com>
2014-08-23 21:32:24 -0400

    removal

--- server.old.R
+++
@@ -1,8 +0,0 @@
-shinyServer(function(input, output) {
-    output$plot1 <- renderPlot({
-        plot(x,y,pch=17,col="blue",cex=.5)
-    })
-    output$text <- renderText({
-        paste("This is a", input$select," wave.")
-    })
-})
